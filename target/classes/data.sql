707fb401c47db6449b94b2bdff67fb9084c82a19
David <david.bertin@croptracker.com>
2021-11-19 21:56:20 -0500

    Step 1 - update the database

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -11,17 +11,17 @@ INSERT INTO AUTHOR(AUTHOR_ID, FIRST_NAME, LAST_NAME)
     (     NULL, 'Walter','Savich'  ),
     (     NULL, 'John','Carnell'   );
 
-INSERT INTO BOOK (BOOK_ID, BOOK_TITLE, BOOK_YEAR, AUTHOR_ID)
+INSERT INTO BOOK (BOOK_ID, BOOK_TITLE, BOOK_YEAR, AUTHOR_ID, BOOK_PRICE)
 VALUES 
-    (NULL, 'Spring Boot in Action', 2018, (SELECT Author_ID from author where last_name = 'Wells')),
-    (NULL, 'Core Java for the Impatient', 2018, (SELECT Author_ID from author where last_name = 'Horstmann')),
-    (NULL, 'Spring Microservices in Action', 2016, (SELECT Author_ID from author where last_name = 'Carnell')),
-    (NULL, 'Java Programming', 2018, (SELECT Author_ID from author where last_name = 'Farrell')),
-    (NULL, 'Beginning Java Databases', 2001, (SELECT Author_ID from author where last_name = 'Carnell')),
-    (NULL, 'Effective Java', 2017, (SELECT Author_ID from author where last_name = 'Bloch')),
-    (NULL, 'Spring Boot 2.0', 2017, (SELECT Author_ID from author where last_name = 'Antonov')),
-    (NULL, 'Spring Boot Cookbook', 2015, (SELECT Author_ID from author where last_name = 'Antonov')),
-    (NULL, 'Learning spring Boot 2.0', '2017', (SELECT Author_ID from author where last_name = 'Turnquist')),
-    (NULL, 'Absolute Java', '2015', (SELECT Author_ID from author where last_name = 'Savich')),
-    (NULL, 'Agile Testing', '2015', (SELECT Author_ID from author where last_name = 'Gregory')),
-    (NULL, 'Java: An Introduction to Problem Solving', '2017', (SELECT Author_ID from author where last_name = 'Savich'));
+    (NULL, 'Spring Boot in Action', 2018, (SELECT Author_ID from author where last_name = 'Wells'), 10.99),
+    (NULL, 'Core Java for the Impatient', 2018, (SELECT Author_ID from author where last_name = 'Horstmann'), 11.99),
+    (NULL, 'Spring Microservices in Action', 2016, (SELECT Author_ID from author where last_name = 'Carnell'), 10.99),
+    (NULL, 'Java Programming', 2018, (SELECT Author_ID from author where last_name = 'Farrell'), 11.99),
+    (NULL, 'Beginning Java Databases', 2001, (SELECT Author_ID from author where last_name = 'Carnell'), 10.99),
+    (NULL, 'Effective Java', 2017, (SELECT Author_ID from author where last_name = 'Bloch'), 11.99),
+    (NULL, 'Spring Boot 2.0', 2017, (SELECT Author_ID from author where last_name = 'Antonov'), 10.99),
+    (NULL, 'Spring Boot Cookbook', 2015, (SELECT Author_ID from author where last_name = 'Antonov'), 11.99),
+    (NULL, 'Learning spring Boot 2.0', '2017', (SELECT Author_ID from author where last_name = 'Turnquist'), 10.99),
+    (NULL, 'Absolute Java', '2015', (SELECT Author_ID from author where last_name = 'Savich'), 11.99),
+    (NULL, 'Agile Testing', '2015', (SELECT Author_ID from author where last_name = 'Gregory'), 10.99),
+    (NULL, 'Java: An Introduction to Problem Solving', '2017', (SELECT Author_ID from author where last_name = 'Savich'), 11.99);
